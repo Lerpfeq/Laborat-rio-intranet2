103db5871e4d3ff19c0fee7204d81d2df91d6176
Lerpfeq <lerpfeq@gmail.com>
2026-04-21 21:11:03 -0300

    Update route.ts

--- nextjs_space/app/api/signup/route.ts
+++ nextjs_space/app/api/signup/route.ts
@@ -33,7 +33,6 @@ export async function POST(request: NextRequest) {
         password: hashedPassword,
         status: "approved",
         category: "admin",
-        role: "admin",
       },
     });
 
